2a40e76182beaaea0940ef7e9c9b83ff26750e53
mdraft <mdraft@yandex.ru>
2016-04-01 22:33:37 +0500

    Added files via upload

---
+++ congr2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+<title>Реал +</title>
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000" link="#000000">
+<?php 
+mail("al.lubnin@gmail.com", "Письмо с real72" ,"Написал(а) тебе: ".$_POST['to']."\nПочтовый адрес:".$_POST['pochta']."\nПожелания:".$_POST['body']);
+?>
+      <p>&nbsp;</p>
+      <p align="center"><a href="http://real72.ru"><img src="/real72_logo.png" width="186" height="64" border="0"></a></p>
+      <p align="center">Спасибо! Мы обработаем заказ и свяжемся с Вами</p>
+      <p align="center"><a href="http://real72.ru">Обратно на главную</a></p>
+</body>
+</html>
